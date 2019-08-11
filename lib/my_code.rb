244974aa92ac2bc8d8c2aaf5439b636ca05146c7
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-11 17:37:37 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
-# Your Code Here
+require 'pry'
+
+def map(array)
+  i = 0
+  arr = []
+  while i < array.length
+    arr << yield(array[i])
+    i = i + 1
+  end
+
+  return arr
+end
+
+def reduce(array, start = nil)
+  i = 0
+  
+  if start != nil
+    value = start
+  else
+    value = array[0]
+    i = 1 
+  end
+  
+  while i < array.length
+    value = yield(value, array[i])
+    i = i + 1
+  end
+  
+  return value
+  
+end 
