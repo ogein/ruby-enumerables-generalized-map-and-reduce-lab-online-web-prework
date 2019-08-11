27b943b361fa44bd6bd5587b8b0cff26cd7e3f2c
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-11 17:07:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
-# Your Code Here
+def map(array)
+  i = 0
+  arr = []
+  while i < array.length
+    arr << yield(array[i])
+    i = i + 1
+  end
+
+  return arr
+end
+
+def reduce(array)
+  
+  arr = []
+  while i < array.length
+    arr << yield(array[i], num)
+    i = i + 1
+  end
+
+  return arr
+end 
